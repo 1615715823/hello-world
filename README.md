195183a5dbf6b06d219a4925efe8eceee4f14df1
tongye <1615715823@qq.com>
2021-11-30 15:44:43 +0800

    1.eureka服务注册中心

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # hello-world
 入门练习库
 hello world 001
+
+# 2021.11.30
+eureka服务注册中心
---
+++ eureka-security-server/src/main/java/com/tongye/cloud/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.tongye.cloud.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * <p>
+ *
+ * <p>
+ *
+ * @Author zhuhanqing
+ * @Date 2021/11/24 16:10
+ * @Version 1.0.0
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
